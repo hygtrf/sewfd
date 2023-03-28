0eded0cdeafb0688c0e6253b107ee790f91841f6
hygtrf <129146638+hygtrf@users.noreply.github.com>
2023-03-28 06:06:01 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=7f782671-a39f-4b9f-b878-2712aee30dbc
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=arsenalfclatestnews.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
